0d2e775aad26af8005389ffc7f0a22cb99e49007
Mohamed Medhat <mohamedsheetat@gmail.com>
2025-11-14 15:33:52 +0200

    fix timeout problem

--- server/config/db.js
+++ server/config/db.js
@@ -5,6 +5,7 @@ async function connectToDb()
 {
     try{
         await mongoose.connect(process.env.MONGO_CLOUD_URI , {
+            serverSelectionTimeoutMS: 30000, 
         });
         console.log("connected to mongoDB.");
     }
